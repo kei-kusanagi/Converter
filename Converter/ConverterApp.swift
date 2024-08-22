8adc5371fea93264da8730603cb87f5ed41ba417
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-22 17:52:39 -0600

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Juan Carlos Robledo Morales on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
